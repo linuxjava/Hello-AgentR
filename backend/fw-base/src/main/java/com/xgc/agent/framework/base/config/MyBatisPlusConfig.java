22a1a9a470dee57242a08720ef8c43d787bb8435
xiaoguochang <xiaoguochang>
2026-08-06 11:49:38 +0800

    优化基础架构

--- backend/fw-base/src/main/java/com/xgc/agent/framework/base/config/MyBatisPlusConfig.java
+++ backend/fw-base/src/main/java/com/xgc/agent/framework/base/config/MyBatisPlusConfig.java
@@ -15,12 +15,13 @@
  * limitations under the License.
  */
 
-package com.xgc.agent.framework.base.mybatisplus;
+package com.xgc.agent.framework.base.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.xgc.agent.framework.base.mybatisplus.MyBatisPlusMetaObjectHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
